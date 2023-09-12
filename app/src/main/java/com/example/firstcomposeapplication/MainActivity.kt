18c7ab07cab19dd9d25841f8d5f88d28162d17a2
Saad-Zaman1 <saad.zaman5439@gmail.com>
2023-09-12 18:14:20 +0500

    Adding space between columns and rows

--- app/src/main/java/com/example/firstcomposeapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstcomposeapplication/MainActivity.kt
@@ -6,13 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -45,20 +50,40 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String,name2:String, modifier: Modifier = Modifier) {
-    LazyRow (
-
-        modifier = Modifier
-            .fillMaxSize(),
-
-    ){
-        items(2){
-
-            Icon(imageVector = Icons.Default.Add,
-                contentDescription = null,
-                modifier = Modifier.size(90.dp))
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        LazyRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp) // Add padding to create space above the LazyRow
+        ) {
+            items(2) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = null,
+                    modifier = Modifier.size(90.dp)
+                )
+            }
         }
 
+        Spacer(modifier = Modifier.height(16.dp)) // Add space between the LazyRow and LazyColumn
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp) // Add padding to create space below the LazyColumn
+        ) {
+            items(2) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = null,
+                    modifier = Modifier.size(90.dp)
+                )
+            }
+        }
     }
+
 }
 
 @Preview(showBackground = true)
