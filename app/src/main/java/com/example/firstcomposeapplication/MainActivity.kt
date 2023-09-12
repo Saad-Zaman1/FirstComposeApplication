f18ff5a74c11de9edf2f65504bf94e398d0449f0
Saad-Zaman1 <saad.zaman5439@gmail.com>
2023-09-12 17:36:45 +0500

    Initial commit

---
+++ app/src/main/java/com/example/firstcomposeapplication/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.firstcomposeapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.firstcomposeapplication.ui.theme.FirstComposeApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FirstComposeApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+//                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String,name2:String, modifier: Modifier = Modifier) {
+    LazyRow (
+
+        modifier = Modifier
+            .fillMaxSize(),
+
+    ){
+        items(2){
+
+            Icon(imageVector = Icons.Default.Add,
+                contentDescription = null,
+                modifier = Modifier.size(90.dp))
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FirstComposeApplicationTheme {
+        Greeting("Saad","Zaman")
+    }
+}
